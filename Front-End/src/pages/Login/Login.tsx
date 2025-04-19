126066863f9a0e5aed96aeb9055a187bdc094053
Gekyume Serna <contatogekyume@gmail.com>
2025-04-19 15:41:44 -0300

    começa tela de login

--- Front-End/src/pages/Login/Login.tsx
+++ Front-End/src/pages/Login/Login.tsx
@@ -1,6 +1,28 @@
+import "./login.scss";
+
 function Login() {
-    return <h1>Login/Register</h1>;
-  }
-  
-  export default Login;
-  
+    return (
+        <section id="login">
+            <form action="">
+                <h1>LOGIN</h1>
+
+                <fieldset>
+                    <label htmlFor="name">NOME DE USUARIO</label>
+                    <input type="text" name="name" id="name" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="email">EMAIL</label>
+                    <input type="email" name="email" id="email" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="password">SENHA</label>
+                    <input type="password" name="password" id="password" />
+                </fieldset>
+
+                <button>Login</button>
+            </form>
+        </section>
+    );
+}
+
+export default Login;
