c8044f2658000f09024aeaf2a6686f4980842519
Gekyume Serna <contatogekyume@gmail.com>
2025-05-16 13:15:24 -0300

    deploy com docker do render

--- FrontEnd/src/pages/Chat/Chat.tsx
+++ FrontEnd/src/pages/Chat/Chat.tsx
@@ -1,8 +1,21 @@
-import "./Chat.scss"
+import "./Chat.scss";
 function Chat() {
     return (
         <section id="chat">
             <h1>Área de Chat</h1>
+
+            <div className="message-container">
+
+                <div className="message">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, animi porro repudiandae consectetur fugiat numquam repellat obcaecati incidunt dolore ducimus iusto asperiores beatae quidem excepturi laudantium enim. Dolor, beatae odit.
+                </div>
+                <div className="message">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, animi porro repudiandae consectetur fugiat numquam repellat obcaecati incidunt dolore ducimus iusto asperiores beatae quidem excepturi laudantium enim. Dolor, beatae odit.
+                </div>
+            </div>
+
+            <input type="text" />
+            
         </section>
     );
 }
